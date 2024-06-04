3401deb04a9e8df59ea4bd7d3d1df870cc7a2d1e
Vadym <telcom.odessa@gmail.com>
2024-06-04 22:33:36 +0300

    Initial

---
+++ module_10-1.py
@@ -0,0 +1,23 @@
+
+import random
+import time
+from threading import Thread
+
+Fig = [1,2,3,4,5,6,7,8,9,10]
+lit = ['a','b','c','d','e','f','h','i','j','k']
+print(Fig)
+print(lit)
+
+def printing_list(list):
+    for i in range(len(list)):
+        _ = 3**(random.randrange(50, 70) * 10000) # имитация работы программы
+        time.sleep(1)        # задержка выдачи на печать 1 сек.
+        print(list[i])
+
+
+thread = Thread(target = printing_list,kwargs=dict(list=Fig))
+thread.start()
+
+printing_list(lit)
+
+thread.join()
