99233c6c04713e00c9cbaed0084d2edfd20c5ae7
Vadym <telcom.odessa@gmail.com>
2024-06-04 22:47:13 +0300

    Initial

--- module_10-1.py
+++ module_10-1.py
@@ -5,8 +5,8 @@ from threading import Thread
 
 Fig = [1,2,3,4,5,6,7,8,9,10]
 lit = ['a','b','c','d','e','f','h','i','j','k']
-print(Fig)
-print(lit)
+print('Входной список цифр:',Fig)
+print('Входной список букв:',lit)
 
 def printing_list(list):
     for i in range(len(list)):
